98487e1a3836902286a0212a3bda8a790b57aff3
Ignacio Toledo <itoledoc@gmail.com>
2015-11-12 23:24:19 -0300

    update

--- WtoAlgorithm3.py
+++ WtoAlgorithm3.py
@@ -4,14 +4,17 @@ import os
 import wto3_tools as wtool
 
 from WtoDataBase3 import WtoDatabase3
-# from astropy import units as u
-# from astropy.coordinates import SkyCoord, EarthLocation, AltAz
-# from astropy.time import Time
+from astropy import units as u
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+from astropy.time import Time
 
 
 # noinspection PyUnresolvedReferences
-# ALMA = EarthLocation(
-#     lat=-23.0262015*u.deg, lon=-67.7551257*u.deg, height=5060*u.m)
+ALMA = EarthLocation(
+    lat=-23.0262015*u.deg, lon=-67.7551257*u.deg, height=5060*u.m)
+TIME = Time.now()
+TIME.delta_ut1_utc = 0
+TIME.location = ALMA
 
 ALMA1 = ephem.Observer()
 ALMA1.lat = '-23.0262015'
@@ -57,9 +60,18 @@ class WtoAlgorithm3(WtoDatabase3):
             index=['ALMA_RB_06', 'ALMA_RB_03', 'ALMA_RB_07', 'ALMA_RB_09',
                    'ALMA_RB_04', 'ALMA_RB_08', 'ALMA_RB_10'])
         self.reciever['g'] = [0., 0., 0., 1., 0., 0., 1.]
+        self._pwv = None
+        self._array_res = []
+        self._date = ephem.now()
+        self._availableobs = False
+        self._time_astropy = TIME
+        self._ALMA_ephem = ALMA1
 
-    def sched_db(self):
-        pass
+    def set_time_now(self):
+        self._time_astropy = Time.now()
+        self._time_astropy.delta_ut1_utc = 0
+        self._time_astropy.location = ALMA
+        self._ALMA_ephem.date = ephem.now()
 
     def write_ephem_coords(self):
 
@@ -77,7 +89,8 @@ class WtoAlgorithm3(WtoDatabase3):
 
         results = ephem_sb.apply(
             lambda x: wtool.calc_ephem_coords(
-                x['solarSystem'], x['ephemeris'], x['SB_UID']),
+                x['solarSystem'], x['ephemeris'], x['SB_UID'],
+                alma=self._ALMA_ephem),
             axis=1)
 
         for r in results.iteritems():
@@ -88,6 +101,104 @@ class WtoAlgorithm3(WtoDatabase3):
 
         self.obs_param = self.schedblocks.apply(
             lambda r: wtool.observable(
-                r['RA'], r['DEC'], wtool.ALMA1, r['RA'], r['minAR'],
+                r['RA'], r['DEC'], self._ALMA_ephem, r['RA'], r['minAR'],
                 r['maxAR'], r['array'], r['SB_UID'], horizon=horizon), axis=1
         )
+
+    def selector(self, array_kind='TWELVE-M',
+                 prj_status=("Ready", "InProgress"),
+                 sb_status=("Ready", "Suspended", "Running", "CalibratorCheck",
+                            "Waiting"),
+                 cycle=("2013.A", "2013.1", "2015.1", "2015.A"),
+                 letterg=("A", "B", "C"),
+                 bands=("ALMA_RB_03", "ALMA_RB_04", "ALMA_RB_06", "ALMA_RB_07",
+                        "ALMA_RB_08", "ALMA_RB_09", "ALMA_RB_10"),
+                 pads=(), elev=20., minha=-3., maxha=3., site=ALMA, time=None,
+                 min_trans=(0.7, 0.5), pwv=0, conf=None, check_count=True):
+
+        if time:
+            pass
+        time = self._time_astropy
+        print time
+
+
+        # select array kind
+        self.selected = self.schedblocks.query('array == @array_kind').copy()
+
+        # select status
+        # select Cycle, Letter
+
+        prj_uid_s = self.projects.query(
+            'PRJ_STATUS in @prj_status and CYCLE in @cycle and '
+            'PRJ_LETTER_GRADE in @letterg').OBSPROJECT_UID.unique()
+        sbs_uid_s = self.sb_status.query(
+            'SB_STATE in @sb_status').SB_UID.unique()
+
+        self.selected = self.selected.query(
+            'OBSPROJECT_UID in @prj_uid_s and SB_UID in @sbs_uid_s').copy()
+
+        # select band
+        self.selected = self.selected.query(
+            'band in @bands'
+        ).copy()
+
+        qastatus = self.aqua_execblock.query(
+            'SB_UID in @sbs_uid_s').groupby(
+            ['SB_UID', 'QA0STATUS']).QA0STATUS.count().unstack().fillna(0)
+        self.selected = pd.merge(
+            self.selected, qastatus.reset_index(),
+            left_index=True, right_on='SB_UID', how='left'
+        ).fillna(0).set_index('SB_UID', drop=False)
+        self.selected = pd.merge(self.selected, self.sb_status, on='SB_UID')
+        if 'Pass' not in self.selected.columns.values:
+            self.selected['Pass'] = 0
+        if 'Unset' not in self.selected.columns.values:
+            self.selected['Unset'] = 0
+
+        self.selected['Observed'] = self.selected.Unset + self.selected.Pass
+
+        # select still needs observations
+        if check_count:
+            self.selected = self.selected.query('EXECOUNT > Pass')
+
+        # select array resolution
+        self.selected = pd.merge(
+            self.selected, self.obs_param, on='SB_UID').set_index('SB_UID',
+                                                                  drop=False)
+        if conf:
+            qstring = ''
+            l = len(conf) - 1
+            for i, c in enumerate(conf):
+                col = c.replace('-', '_')
+                if i == l:
+                    qstring += '%s == "%s"' % (col, c)
+                else:
+                    qstring += '%s == "%s" or ' % (col, c)
+            self.selected = self.selected.query(qstring)
+
+        # select observable: elev, ha, moon & sun distance
+        try:
+            c = SkyCoord(
+                ra=self.selected.RA*u.degree,
+                dec=self.selected.DEC*u.degree,
+                location=site, obstime=time)
+        except IndexError:
+            print("Nothing to observe? %s") % len(self.selected)
+            self._availableobs = False
+            return
+
+        ha = time.sidereal_time('apparent') - c.ra
+        self.selected['HA'] = ha.wrap_at(180*u.degree).value
+        self.selected['RAh'] = c.ra.hour
+        self.selected['elev'] = c.transform_to(
+            AltAz(obstime=time, location=site)).alt.value
+        self.selected = self.selected.query(
+            'elev >= @elev and (@minha <= HA <= @maxha)').copy()
+        self._availableobs = True
+
+        # select transmission
+
+        # calculate frac
+
+        # select frac
+
